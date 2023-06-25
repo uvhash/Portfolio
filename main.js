75ee80cdb98352db69f37621a44eec3e68c80623
Carlos Gonzalez <bcgonzalez333@gmail.com>
2023-06-25 02:33:20 -0700

    made some changes

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-setupCounter(document.querySelector('#counter'))
+// Scene
+// Camera
+// Renderer
+
+// Geometry
+// Material
+// Light
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+renderer.setClearColor(0x1d2225);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(12);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.BoxGeometry(18.981, 8.497, 45, 11, 11, 11);
+//const geometry = new THREE.BoxGeometry(30, 20, 5, 1, 1, 1);
+const geometry2 = new THREE.PlaneGeometry(6.594, 2.94);
+const material = new THREE.MeshBasicMaterial({ color: 0xf5e9c3, wireframe: false });
+const material2 = new THREE.MeshBasicMaterial({ color: 0x1d2225, wireframe: false });
+const cube = new THREE.Mesh(geometry, material);
+const planeCenter = new THREE.Mesh(geometry2, material2);
+
+scene.add(cube);
+scene.add(planeCenter);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+renderer.render(scene, camera);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate()
+
