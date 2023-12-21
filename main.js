546e4cd50c0b231f2dc7ed0e24fb5a0711d8e124
Carlos <bcgonzalez333@gmail.com>
2023-12-20 22:04:50 -0800

    updating before some more changes

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-import './style.css'
+import { gsap } from "gsap";
+import "./style.css";
+
+function enter() {
+  const t = document.getElementById("EnterView");
+  const e = t.querySelector("._t1");
+  const n = t.querySelector("._t2");
+
+  gsap.delayedCall(1, function () {
+    gsap.to(e, {
+      opacity: 0,
+      duration: 0.6,
+    });
+
+    gsap.to(n, {
+      opacity: 0,
+      duration: 0.6,
+      delay: 0.15,
+    });
+
+    gsap.delayedCall(0.6, function () {
+      gsap.to(t, {
+        opacity: 0,
+        duation: 1.6,
+        onComplete: function () {
+          t.remove();
+        },
+      });
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", enter);
